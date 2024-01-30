fcb3beceaced72cd8ab2037f26d6b84824ade446
Anne Hastings <anne.miriam@gmail.com>
2024-01-30 12:07:07 -0700

    initial creation files

---
+++ server/app.py
@@ -0,0 +1,178 @@
+# Remote library imports
+from flask import Flask, request, make_response, session
+from flask_restful import Resource, Api
+from os import environ
+from dotenv import load_dotenv
+from flask_marshmallow import Marshmallow
+
+# from flask_bcrypt import Bcrypt
+
+# Local imports
+from config import app, db, api
+
+# Secret Key
+load_dotenv(".env")
+app.secret_key = environ.get("SECRET_KEY")
+
+# Add your model imports
+from models import User, Item, Collection, Comment, Forum
+ma = Marshmallow(app)
+
+# Home Page
+@app.route("/")
+def index():
+    return "<h1>Time Capsule Treasures</h1>"
+
+######################Login/SignUp/CheckSession ##########################
+
+class Signup(Resource):
+    def post(self):
+        try:
+            new_user = User(
+                username = request.json['username'],
+                email = request.json['email'],  
+                first_name = request.json['first_name']
+            )
+            new_user.password_hash = request.json["password"]
+            db.session.add(new_user)
+            db.session.commit()
+            session["user_id"] = new_user.id
+            return make_response(new_user.to_dict(), 201)
+        except ValueError as e:
+            return make_response({"error": f"{e}"}, 400)
+
+
+class Login(Resource):
+    def post(self):
+        username = request.json["username"]
+        password = request.json["password"]
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
+            session["user_id"] = user.id
+            return user.to_dict(), 200
+        session.clear()
+        return {"error": "Incorrect username or password"}, 401
+
+
+class Logout(Resource):
+    def delete(self):
+        session.clear()
+        return {}, 204
+
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.get(session.get("user_id"))
+        if user:
+            return user.to_dict(), 200
+        else:
+            return {}, 401
+
+
+################################# User #################################
+
+
+class Users(Resource):
+    def get(self):
+        try:
+            return make_response([user.to_dict() for user in User.query.all()], 200)
+        except Exception as e:
+            return make_response({"Error": "Could not get data"}, 400)
+
+
+class UsersById(Resource):
+    def get(self, id):
+        user = User.query.get(id)
+        if user:
+            return make_response(user.to_dict(), 200)
+        return make_response({"error": "User not found"}, 404)
+
+########################## Item ##############################
+
+
+class Items(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class ItemById(Resource):
+    def patch(self,id):
+        pass
+
+    def delete(self,id):
+        pass
+
+############################ Collection ############################
+
+
+class Collections(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class CollectionById(Resource):
+    def patch(self,id):
+        pass
+
+    def delete(self,id):
+        pass
+
+############################# Comment #############################
+
+
+class Comments(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class CommentById(Resource):
+    def delete(self,id):
+        pass
+
+################################ Forum ######################################
+
+
+class Forums(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class ForumById(Resource):
+    def patch(self,id):
+        pass
+
+    def delete(self,id):
+        pass
+
+
+api.add_resource(Signup, "/sign_up")
+api.add_resource(Login, "/login")
+api.add_resource(Logout, "/logout")
+api.add_resource(CheckSession, "/check_session")
+api.add_resource(Users, "/user")
+api.add_resource(UsersById, "/user/<int:id>")
+api.add_resource(Items, "/items")
+api.add_resource(ItemById, "/items/<int:id>")
+api.add_resource(Collections, "/collections")
+api.add_resource(CollectionById, "/collections/<int:id>")
+api.add_resource(Comments, "/comments")
+api.add_resource(CommentById, "/comments/<int:id>")
+api.add_resource(Forums, "/forums")
+api.add_resource(ForumById, "forums/<int:id>")
+
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
---
+++ server/models.py
@@ -0,0 +1,144 @@
+# from sqlalchemy_serializer import SerializerMixin
+# from sqlalchemy.ext.associationproxy import association_proxy
+# from sqlalchemy.ext.hybrid import hybrid_property
+# from sqlalchemy.orm import validates
+
+
+from config import *
+
+# password encryption
+import bcrypt  
+
+######################## classes ####################################
+# User
+class User(db.Model):
+    __tablename__ = "users"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    first_name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)  
+    _password_hash = db.Column(db.String, nullable=False)
+
+    # relationships
+    # collections = db.relationship("Collection", back_populates="user", cascade="all, delete-orphan")
+    # items = association_proxy("collections", "item")
+
+    # Serializer
+    # serialize_rules = ('-_password_hash', 'collections')
+
+    # Validation
+
+    # @hybrid_property
+    # def password_hash(self):
+    #     raise AttributeError("Password hashes are private.")  
+    #     # return self._password_hash
+
+    # @password_hash.setter
+    def password_set(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
+        self._password_hash = password_hash.decode("utf-8")
+        # hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())          
+        # self._password_hash = hashed.decode("utf-8")
+
+    def password_check(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
+        # return bcrypt.checkpw(password.encode("utf-8"), self._password_hash.encode("utf-8"))  
+
+    def __repr__(self):
+        return f"User {self.username}"
+    
+
+
+# Item
+class Item(db.Model):
+    __tablename__ = 'items'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    trade_status = db.Column(db.Boolean, nullable=False)
+    ebay_link = db.Column(db.String)
+    decade = db.Column(db.Integer)
+
+    # Relationships
+    # collections = db.relationship("Collection", back_populates="user", cascade="all, delete-orphan")
+    # users = association_proxy("trips", "user")
+
+    # Serializer
+    # serialize_rules = ('-collections',)
+
+    # Validation
+    
+
+    def __repr__(self):
+            return f"Item {self.name}"
+
+
+# Collection => many-to-many
+class Collection(db.Model):
+    __tablename__ = 'collections'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    # Relationships
+    # user = db.relationship("User", back_populates="collections")
+    # item = db.relationship("Item", back_populates="collections")
+
+    # Serializer
+    # serialize_rules = ('-user', '-item')
+
+    # Validation
+    # @validates("user_id", "destination_id")
+    # def validate_ids(self, key, id):
+    #     if not isinstance(id, int):
+    #         raise ValueError(f"{key} must be an integer.")
+    #     return id
+
+    def __repr__(self):
+        return f"Collection {self.name}"
+
+
+# Comment => many-to-many
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    # Relationships
+    
+
+    # Serializer
+    
+
+    # Validation
+    
+
+    def __repr__(self):
+        return "Comment"
+
+
+# Forum => one-to-many
+class Forum(db.Model):
+    __tablename__ = 'forums'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    # Relationships
+    
+
+    # Serializer
+    
+
+    # Validation
+    
+
+    def __repr__(self):
+        return "Forum"
